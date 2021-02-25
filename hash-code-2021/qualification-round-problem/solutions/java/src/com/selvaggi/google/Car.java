61af2b16b17e83ea84c10d09fd624cee55000137
Bock92 <boccato92@gmail.com>
2021-02-25 19:33:04 +0100

    Add Car

---
+++ hash-code-2021/qualification-round-problem/solutions/java/src/com/selvaggi/google/Car.java
@@ -0,0 +1,14 @@
+package com.selvaggi.google;
+
+import java.util.ArrayList;
+
+public class Car {
+    public int id;
+    public ArrayList<Street> streets;
+    public int tripTime;
+    public boolean isArrived;
+
+    public Car() {
+        
+    }
+}
