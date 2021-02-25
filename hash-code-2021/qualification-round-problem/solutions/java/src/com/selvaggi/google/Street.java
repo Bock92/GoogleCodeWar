330be8b4d64c14828af71fd76b41f4855eba755b
Bock92 <boccato92@gmail.com>
2021-02-25 19:23:43 +0100

    Add street

---
+++ hash-code-2021/qualification-round-problem/solutions/java/src/com/selvaggi/google/Street.java
@@ -0,0 +1,13 @@
+package com.selvaggi.google;
+
+public class Street {
+    public int id;
+    public String name;
+    public int length;
+    public int start_intersection;
+    public int end_intersection;
+
+    public Street() {
+
+    }
+}
