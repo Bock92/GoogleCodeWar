f76c2b50f816bbcffe1c8105a385ce50ba5681f8
maubaga <maubaga92@gmail.com>
2021-02-25 20:03:08 +0100

    Input read complete

--- hash-code-2021/qualification-round-problem/solutions/java/src/com/selvaggi/google/Qualification2021.java
+++ hash-code-2021/qualification-round-problem/solutions/java/src/com/selvaggi/google/Qualification2021.java
@@ -14,11 +14,17 @@ public class Qualification2021 {
 	public static final int FLEXIBLE_NUMBER = 0;
 	public static final int MIN_CUT = 1100;
 
-	static int pizzaNumber =0;
-	static int twoMemberTeam = 0;
-	static int threeMemberTeam = 0;
-	static int fourMemberTeam = 0;
-	static int maxNumOfIngredient = 0;
+	static int simulationDuration = 0;
+	static int numberOfIntersection = 0;
+	static int numberOfStreets = 0;
+	static int numberOfCars = 0;
+	static int bonusPoint = 0;
+
+
+	static ArrayList<Street> streets;
+	static ArrayList<Car> cars;
+	static World world;
+
 
 	/**
 	 * Read input file
@@ -26,9 +32,12 @@ public class Qualification2021 {
 	 * @param fileName
 	 * @return ArrayList with the input structured
 	 */
-	private static ArrayList<Pizza> readInputFile(String filePath, String fileName){
+	private static void readInputFile(String filePath, String fileName){
 		System.out.println("Read file named " + fileName);
-		ArrayList<Pizza> pizzas = new ArrayList<>();
+		//ArrayList<Pizza> pizzas = new ArrayList<>();
+
+		streets = new ArrayList<>();
+		cars = new ArrayList<>();
 
 		try {
 			File myObj = new File(filePath + fileName);
@@ -39,39 +48,59 @@ public class Qualification2021 {
 			if(myReader.hasNextLine()){
 				String data = myReader.nextLine();
 				Scanner myLine = new Scanner(data);
-				pizzaNumber = myLine.nextInt();
-				twoMemberTeam = myLine.nextInt();
-				threeMemberTeam = myLine.nextInt();
-				fourMemberTeam = myLine.nextInt();
+				simulationDuration = myLine.nextInt();
+				numberOfIntersection = myLine.nextInt();
+				numberOfStreets = myLine.nextInt();
+				numberOfCars = myLine.nextInt();
+				bonusPoint = myLine.nextInt();
 				myLine.close();
 			}
 
-
-			// Loop through the lines
-			while (myReader.hasNextLine()) {
+			// Read the streets
+			for(int i = 0; i < numberOfStreets; i++){
 				String data = myReader.nextLine();
 				Scanner myLine = new Scanner(data);
 
-				// Read number of ingredients
-				int numOfIngredient = myLine.nextInt();
-				if(numOfIngredient > maxNumOfIngredient)
-					maxNumOfIngredient = numOfIngredient;
+				Street myStreet = new Street();
 
-				Pizza myPizza = new Pizza(numOfIngredient);
-				myPizza.setIndex(pizzas.size());
+				myStreet.start_intersection = myLine.nextInt();
+				myStreet.end_intersection = myLine.nextInt();
+				myStreet.name = myLine.next();
+				myStreet.length = myLine.nextInt();
+				myStreet.id = i;
 
-				// Loop through the tokens
-				while(myLine.hasNext())
-					myPizza.addIngredient(myLine.next());
-				myLine.close();
-				pizzas.add(myPizza);
+				streets.add(myStreet);
+				world.addStreet(myStreet);
+			}
+
+			// Read the cars
+			for(int i = 0; i < numberOfCars; i++){
+				String data = myReader.nextLine();
+				Scanner myLine = new Scanner(data);
+
+				Car myCar = new Car();
+
+				myCar.streetsSize = myLine.nextInt();
+
+				ArrayList<Street> path = new ArrayList<>();
+
+
+
+				while(myLine.hasNext()) {
+					String newStreet = myLine.next();
+					path.add(world.getStreetByName(newStreet));
+				}
+
+				myCar.streets = path;
+
+				cars.add(myCar);
+
 			}
 			myReader.close();
 		}
 		catch (FileNotFoundException e) {
 			System.out.println("File Not found! " + filePath + fileName);
 		}
-		return pizzas;
 	}
 
 	/**
@@ -102,204 +131,32 @@ public class Qualification2021 {
 
 		String filePath = "..\\..\\input_files\\";
 		String outPath = "..\\..\\output\\";
-		String fileName = "a_example";
+		String fileName = "a.txt";
 
 		if(args.length > 0)
 			fileName = args[0];
 
+		world = new World();
 		// Read the input file
-		ArrayList<Pizza> pizzas = readInputFile(filePath, fileName);
+		readInputFile(filePath, fileName);
 
-		// Print file statistics
-		System.out.println("Team 2: " + twoMemberTeam + " Team 3: "+ threeMemberTeam + " Team 4: " + fourMemberTeam);
-		System.out.println("Number of pizzas: " + pizzas.size() + " Max Ingredients: " + maxNumOfIngredient);
+		// Print input
+		System.out.println("simulationDuration " + simulationDuration+
+		" numberOfIntersection " + numberOfIntersection+
+		" numberOfStreets " +numberOfStreets+
+		" numberOfCars " +numberOfCars+
+		" bonusPoint " +bonusPoint );
 
-		//////////////////////////////////////////////////////////
-		/******************* END READ FILE *********************/
-		//////////////////////////////////////////////////////////
-
-		// Sort pizzas by ingredients
-		Collections.sort(pizzas, new Comparator<Pizza>() {
-			@Override
-			public int compare(Pizza p2, Pizza p1)
-			{
-				return  p1.numberOfIngredients - p2.numberOfIngredients;
-			}
-		});
-
-
-		// First Cut
-		int firstCut = Math.min(twoMemberTeam * 2 + threeMemberTeam  * 3 + fourMemberTeam * 4 + FLEXIBLE_NUMBER, MIN_CUT);
-
-		for (int i = pizzas.size()-1; i >= firstCut; i--)
-			pizzas.remove(i);
-
-		pizzaNumber = pizzas.size();
-
-		System.out.println("Working on 2 combinations - First cut " + firstCut);
-		// ALL 2 COMBINATION
-		ArrayList<Delivery> twoDeliveries = new ArrayList<>();
-
-		if(twoMemberTeam != 0){
-			for(int i = 0; i < pizzas.size(); i++){
-				for(int j = i + 1; j< pizzas.size(); j++){
-					Delivery myDelivery = new Delivery();
-					myDelivery.addPizza(pizzas.get(i));
-					myDelivery.addPizza(pizzas.get(j));
-					twoDeliveries.add(myDelivery);
-				}
-			}
+		for (Street s: streets) {
+			System.out.println(s.toString());
+		}
+		for (Car c: cars) {
+			System.out.println(c.toString());
 		}
 
-		Collections.sort(twoDeliveries, new Comparator<Delivery>() {
-			@Override
-			public int compare(Delivery d2, Delivery d1)
-			{
-				return  d1.getScore() - d2.getScore();
-			}
-		});
-
-
-
-
-		// SECOND CUT
-		int secondCut = Math.min(Math.max(threeMemberTeam, fourMemberTeam) + FLEXIBLE_NUMBER, twoDeliveries.size());
-		System.out.println("Working on 3 combinations - Second Cut: " + secondCut);
-		ArrayList<Delivery> threeDeliveries = new ArrayList<>();
-
-		if(threeMemberTeam != 0) {
-			for(int i = 0; i < secondCut; i++){
-				for(int j = 0; j < pizzas.size(); j++){
-					if(!twoDeliveries.get(i).containsPizza(pizzas.get(j))){
-						Delivery myDelivery = new Delivery();
-						// Copy deliver
-						for(Pizza p: twoDeliveries.get(i).pizzas) {
-							myDelivery.addPizza(p);
-						}
-						myDelivery.addPizza(pizzas.get(j));
-						threeDeliveries.add(myDelivery);
-					}
-				}
-			}
-		}
-
-		Collections.sort(threeDeliveries, new Comparator<Delivery>() {
-			@Override
-			public int compare(Delivery d2, Delivery d1)
-			{
-				return  d1.getScore() - d2.getScore();
-			}
-		});
-
-
-
-		// ALL 3 COMBINATION
-
-		// THIRD CUT
-		int thirdCut = Math.min(fourMemberTeam + FLEXIBLE_NUMBER, threeDeliveries.size());
-		System.out.println("Working on 4 combinations - Third Cut: " + thirdCut);
-		ArrayList<Delivery> fourDeliveries = new ArrayList<>();
-
-		if(fourMemberTeam != 0) {
-			for(int i = 0; i < thirdCut; i++){
-				for(int j = 0; j < pizzas.size(); j++){
-					if(!threeDeliveries.get(i).containsPizza(pizzas.get(j))){
-						Delivery myDelivery = new Delivery();
-						// Copy deliver
-						for(Pizza p: threeDeliveries.get(i).pizzas) {
-							myDelivery.addPizza(p);
-						}
-						myDelivery.addPizza(pizzas.get(j));
-						fourDeliveries.add(myDelivery);
-					}
-				}
-			}
-		}
-
-		Collections.sort(fourDeliveries, new Comparator<Delivery>() {
-			@Override
-			public int compare(Delivery d2, Delivery d1)
-			{
-				return  d1.getScore() - d2.getScore();
-			}
-		});
-
-
-		/// MIX 2 + 3 +4;
-		ArrayList<Delivery> deliveries = new ArrayList<>();
-
-		for(Delivery d: fourDeliveries)
-			deliveries.add(d);
-		for(Delivery d: threeDeliveries)
-			deliveries.add(d);
-		for(Delivery d: twoDeliveries)
-			deliveries.add(d);
-
-		Collections.sort(deliveries, new Comparator<Delivery>() {
-			@Override
-			public int compare(Delivery d2, Delivery d1)
-			{
-				return  d1.getScore() - d2.getScore();
-			}
-		});
-
-
-		ArrayList<Delivery> finalDeliveries = new ArrayList<>();
-
-		//Get first Delivery
-		finalDeliveries.add(deliveries.get(0));
-		ArrayList<Pizza> pizzaDelivered = new ArrayList<>();
-
-		for (Pizza p: finalDeliveries.get(0).pizzas){
-			pizzaDelivered.add(p);
-		}
-
-
-		pizzaNumber -= deliveries.get(0).getPizzas();
-
-		for (Delivery d : deliveries) {
-			if(twoMemberTeam <= 0 && d.pizzas.size() == 2 || threeMemberTeam <= 0 && d.pizzas.size() == 3 || fourMemberTeam <= 0 && d.pizzas.size() == 4)
-				continue;
-
-			if(d.getPizzas() > pizzaNumber)
-				continue;
-
-			boolean contained = false;
-
-			for(Pizza p: pizzaDelivered){
-				if(d.containsPizza(p)) {
-					contained = true;
-					break;
-				}
-			}
-
-			if(!contained) {
-
-				if(d.pizzas.size() == 2)
-					twoMemberTeam--;
-				if(d.pizzas.size() == 3)
-					threeMemberTeam--;
-				if(d.pizzas.size() == 4)
-					fourMemberTeam--;
-
-				finalDeliveries.add(d);
-				for(Pizza p: d.pizzas)
-					pizzaDelivered.add(p);
-			}
-		}
-
-
-
-		// FINAL OUTPUT
-		System.out.println("FINAL OUTPUT");
-		int finalScore = 0;
-		for(int i = 0; i < finalDeliveries.size(); i++)
-			finalScore += Math.pow(finalDeliveries.get(i).getScore(),2);
-		System.out.println("Final Score: " + finalScore);
-		System.out.println("Number of deliveries: " + finalDeliveries.size());
 
 		// Write the file
-		writeOutputFile(outPath, finalDeliveries);
+		//writeOutputFile(outPath, finalDeliveries);
 
 	}
 }
