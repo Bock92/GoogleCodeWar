d66cde3dad1ca210d24b6d5024586bfce605e985
Bock92 <boccato92@gmail.com>
2021-02-25 19:31:49 +0100

    Add intersection, traffic light

---
+++ hash-code-2021/qualification-round-problem/solutions/java/src/com/selvaggi/google/Intersection.java
@@ -0,0 +1,13 @@
+package com.selvaggi.google;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Intersection {
+    public int id;
+    public ArrayList<TrafficLight> trafficLights;
+
+    public Intersection() {
+
+    }
+}
---
+++ hash-code-2021/qualification-round-problem/solutions/java/src/com/selvaggi/google/TrafficLight.java
@@ -0,0 +1,13 @@
+package com.selvaggi.google;
+
+public class TrafficLight {
+
+    public int id;
+    public boolean isGreen;
+    public int greenTime;
+
+
+    public TrafficLight() {
+
+    }
+}
