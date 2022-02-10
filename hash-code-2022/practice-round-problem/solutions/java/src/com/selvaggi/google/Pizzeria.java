bdfb952322b645273823fa3d596d07343ce38e50
maubaga <maubaga92@gmail.com>
2022-02-10 21:35:50 +0100

    add input

--- hash-code-2022/practice-round-problem/solutions/java/src/com/selvaggi/google/Pizzeria.java
+++ hash-code-2022/practice-round-problem/solutions/java/src/com/selvaggi/google/Pizzeria.java
@@ -3,9 +3,13 @@ package com.selvaggi.google;
 import java.util.ArrayList;
 
 public class Pizzeria {
-    ArrayList<Ingredient> pizzas = new ArrayList<>();
+    public ArrayList<Ingredient> ingredients;
+    public ArrayList<Client> clients;
+    public Pizza pizza;
 
     public Pizzeria(){
-        
+        ingredients = new ArrayList<>();
+        clients = new ArrayList<>();
+        pizza = new Pizza();
     }
 }
